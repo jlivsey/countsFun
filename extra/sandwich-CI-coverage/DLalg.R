fc76f2abeb39347704c3772acfa8dd89f6e82a3b
J. Livsey <james.a.livsey@gmail.com>
2020-05-04 13:25:38 -0400

    sandwich work

---
+++ extra/sandwich-CI-coverage/DLalg.R
@@ -0,0 +1,44 @@
+# Univariate Durbin-Levinson algoithm.
+# two functions. 1 runs algoritm the other
+
+# notes:
+#         1. I'm assuming variance gamma(0) = 1
+#         2. gamma vec being passed does not include variance so g[1] = gamma(1)
+
+DLalg <- function(x, g){
+  # parameters known from inputs
+  n <- length(x)
+
+  # Iniitalize storage
+  phi <- matrix(NA, n, n)
+  v <- rep(NA, n)
+
+  # inital values
+  phi[1, 1] <- g[1]
+  v[1] <- (1 -g[1]^2)
+
+  # main loop
+  for(m in 2:n){
+    phi[m, m] <- (g[m] - sum(phi[m-1, 1:(m-1)] * g[(m-1):1])) / v[m-1]
+    phi[m, 1:(m-1)] <- phi[m-1, 1:(m-1)] - phi[m, m] * phi[m-1, (m-1):1]
+    v[m] <- v[m-1] * (1 - phi[m, m]^2)
+  }
+
+  # Calculate xhat's
+  xhat <- rep(NA, n+1) #storage
+  xhat[1] <- 0
+  for(m in 1:n){
+    xhat[m+1] <- sum(phi[m, 1:m] * x[m:1])
+  }
+
+  # residuals
+  e <- x - xhat[1:n]
+
+  return(list(
+    phi  = phi,
+    v    = v,
+    xhat = xhat,
+    e    = e
+  ))
+
+}
--- extra/sandwich-CI-coverage/sim.R
+++ extra/sandwich-CI-coverage/sim.R
@@ -29,9 +29,24 @@ sim_sandy <- function(lam, phi, n = 100, nsim = 200){
   SE.hess <- sqrt(diag(solve(h)))
 
   # standard erros with sandwhich method
-  M <- (g) %*% t(g) # meat
-  B <- h            # bread
-  SE.sand <- solve(-M) %*% B %*% solve(-M)
+  logfi <- function(theta, idx){
+    LAM <- theta[1]
+    PHI <- theta[2]
+    # HC <- HermCoef(LAM)
+    # ar.acf <- ARMAacf(ar = PHI, lag.max = n)
+    # g <- CountACVF(h = 0:(n-1), myacf = ar.acf, g = HC)
+    g <- ARMAacf(ar = PHI, lag.max = n)[2:(n+1)]
+    DLout <- DLalg(x, g)
+    ei <- DLout$e[idx]
+    vi <- DLout$v[idx]
+    return(-(log(vi) + ei^2/vi)/2)
+  }
+  gi <- matrix(-99, n, 2)
+  for(k in 1:n) gi[k, ] <- grad(func = logfi, x = out$par, idx = k)
+
+  A <- h
+  B <- t(gi) %*% gi
+  SE.sand <- solve(-A) %*% B %*% solve(-A)
 
   # Does true parameter fall in CI
 
