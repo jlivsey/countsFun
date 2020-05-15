42059f64a0e163417559b1acdc89f32fc925b026
J. Livsey <james.a.livsey@gmail.com>
2020-05-15 00:02:45 -0400

    sandwich sim results

--- extra/sandwich-CI-coverage/sim_fixedTheta.R
+++ extra/sandwich-CI-coverage/sim_fixedTheta.R
@@ -5,71 +5,82 @@ library(countsFun)
 library(numDeriv)
 library(sandwich)
 
-
 sim_sandy <- function(lam, phi, n = 100, nsim = 200){
 
-  P <- matrix(NA, nsim, 2) # parameter estimates
-  H <- matrix(NA, nsim, 2) # Std error with hessian
-  S <- matrix(NA, nsim, 2) # std error with Sandwich estimator
+  P <- matrix(NA, nsim, 1) # parameter estimates
+  H <- matrix(NA, nsim, 1) # Std error with hessian
+  S <- matrix(NA, nsim, 1) # std error with Sandwich estimator
 
   for(i in 1:nsim){
 
-  if(i<10 || i%%10==0) print(i)
+    print(i)
+    # if(i%%10==0)print(i)
 
   # simulate data
   x <- sim_pois_ar(n = n, phi = phi, lam = lam)
 
   # set initial values for optim
-  inital.values <- c(jitter(lam), jitter(phi))
+  # inital.values <- c(jitter(lam), jitter(phi))
+  inital.values <- c(jitter(phi))
+
+  HC <- HermCoef(lam)
 
   # run optim
   out <- optim(par     = inital.values,
-               fn      = GaussLogLik,
+               fn      = GaussLogLik_fixedLambda,
+               lam     = lam,
+               HC      = HC,
                data    = x,
                hessian = TRUE)
 
   # Calulate numerical Hessian
-  h <- hessian(func = GaussLogLik, x = out$par, data = x)
+  # h <- hessian(func = GaussLogLik_fixedPhi,
+  #              x = out$par,
+  #              data = x,
+  #              phi_fixed = 0)
+  #
+  # # Calculate numerical gradient
+  # g <- grad(func = GaussLogLik_fixedPhi,
+  #           x = out$par,
+  #           data = x,
+  #           phi_fixed = 0 )
 
-  # Calculate numerical gradient
-  # g <- grad(func = GaussLogLik, x = out$par, data = x)
+  # standard errors usual way using hessian
+  SE.hess <- sqrt(diag(solve(out$hessian)))
 
   # standard erros with sandwhich method
   logfi <- function(theta, idx){
-    LAM <- theta[1]
-    PHI <- theta[2]
+    LAM <- lam
+    PHI <- theta
     HC <- HermCoef(LAM)
     ar.acf <- ARMAacf(ar = PHI, lag.max = n+1)[1:(n+1)]
     g <- CountACVF(h = 0:(n-1), myacf = ar.acf, g = HC)
     # g <- ARMAacf(ar = PHI, lag.max = n)[2:(n+1)]
-    DLout <- DLalg(x, g, LAM)
+    DLout <- DLalg(x, g, lam)
     ei <- DLout$e[idx]
     vi <- DLout$v[idx]
     return(-(log(vi) + ei^2/vi)/2)
   }
-  gi <- matrix(-99, n, 2)
+  gi <- matrix(-99, n, 1)
   for(k in 1:99) gi[k, ] <- grad(func = logfi, x = out$par, idx = k)
-  gi <- gi[-100, ]
+  # AD-HOC REMOVE
+  gi <- gi[-100]
 
-  A <- h
+  A <- out$hessian
   B <- t(gi) %*% gi
   V.sand  <- solve(-A) %*% B %*% solve(-A)
   SE.sand <- sqrt(diag(V.sand))
+  # Does true parameter fall in CI
 
-  # standard errors usual way using hessian
-  SE.hess <- sqrt(diag(solve(h)))
 
   P[i, ] <- out$par
   H[i, ] <- SE.hess
   S[i, ] <- SE.sand
-
   }
 
-  return(list(P = P,
-              H = H,
-              S = S))
-
+  return(list(P=P, H=H, S=S))
 }
+
 
 
 
